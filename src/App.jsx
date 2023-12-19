cb0e0fb9baaabee608329eda2de2a819c940ab0c
elen-oz <ozerova.lenka@gmail.com>
2023-12-19 16:19:55 +0100

    feat: add styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import Header from './Header';
 import Food from './Food';
 import Footer from './Footer';
 
+import styles from './App.module.css';
+
 const App = () => {
   return (
-    <>
+    <div className={styles.app}>
       <Header />
       <Food />
       <Footer />
-    </>
+    </div>
   );
 };
 
