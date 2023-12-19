8e93f0c245910d446052396135d2078c16285ba9
elen-oz <ozerova.lenka@gmail.com>
2023-12-19 14:54:45 +0100

    init: first message

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './Header';
+import Food from './Food';
+import Footer from './Footer';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Food />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; {new Date().getFullYear()} Website Name</p>
+    </footer>
+  );
+};
+
+export default Footer;
